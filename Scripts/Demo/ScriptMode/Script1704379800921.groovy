3e0fab88fcfdac9b6fe03b90d9225b38a22e5924
varunverma123456 <varun.verma15@gmail.com>
2024-01-10 15:17:59 +0530

    commit for change in the user name

--- Scripts/Demo/ScriptMode/Script1704379800921.groovy
+++ Scripts/Demo/ScriptMode/Script1704379800921.groovy
@@ -13,15 +13,22 @@ import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
 import com.kms.katalon.core.testobject.TestObject as TestObject
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import com.kms.katalon.core.webui.keyword.internal.WebUIAbstractKeyword
+import com.kms.katalon.core.webui.keyword.internal.WebUIAbstractKeyword as WebUIAbstractKeyword
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.openBrowser("");
-WebUI.navigateToUrl("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-WebUI.setText(findTestObject('Object Repository/OrangeHRM_TestCase4/Page_OrangeHRM/input_Username_username'), "Admin");
-WebUI.setEncryptedText(findTestObject('Object Repository/OrangeHRM_TestCase4/Page_OrangeHRM/input_Password_password'), "hUKwJTbofgPU9eVlw/CnDQ==");
-WebUI.click(findTestObject('Object Repository/OrangeHRM_TestCase4/Page_OrangeHRM/button_Login'));
-WebUI.verifyTextPresent("Paul Collings", false);
-WebUI.closeBrowser();
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+
+WebUI.setText(findTestObject('Object Repository/OrangeHRM_TestCase4/Page_OrangeHRM/input_Username_username'), 'Admin')
+
+WebUI.setEncryptedText(findTestObject('Object Repository/OrangeHRM_TestCase4/Page_OrangeHRM/input_Password_password'), 'hUKwJTbofgPU9eVlw/CnDQ==')
+
+WebUI.click(findTestObject('Object Repository/OrangeHRM_TestCase4/Page_OrangeHRM/button_Login'))
+
+WebUI.verifyTextPresent('a v', false)
+
+WebUI.closeBrowser()
+
